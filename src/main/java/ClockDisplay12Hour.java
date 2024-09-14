ece30dbcfbaa739dab9725074f137a004235f81b
Solon Chan <147296165+siychan@users.noreply.github.com>
2024-09-14 16:53:04 -0700

    Update ClockDisplay12Hour.java
    removed unnecessary comments from reused code

--- src/main/java/ClockDisplay12Hour.java
+++ src/main/java/ClockDisplay12Hour.java
@@ -6,18 +6,11 @@ public class ClockDisplay12Hour
     //Extend ClockDisplay for a 12 hour clock. This means you need to track AM/PM
     //Update all methods and parameters to accomodate this change
 
-    //Implement 2 private NumberDisplay objects. 
-    //  One for hours and one for minutes
-    //Implement a private String for the display
     private NumberDisplay hours;
     private NumberDisplay minutes;
     private String display;
     private String period;
 
-    //Implement a constructor that takes no parameters
-    //The constructor should set hours as a NumberDisplay object with 24 as the limit
-    //The constructor should set minutes as a NumberDisplay object with 60 as the limit
-    //The constructor should call the method updateDisplay before finishing
     public ClockDisplay12Hour(){
         hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
@@ -26,10 +19,6 @@ public class ClockDisplay12Hour
         updateDisplay();
     }
     
-    //Implement a constructor that takes 2 parameters, hour and minute
-    //The constructor should set hours as a NumberDisplay object with 24 as the limit
-    //The constructor should set minutes as a NumberDisplay object with 60 as the limit
-    //The constructor should call the method setTime with the parameters passed in
     public ClockDisplay12Hour(int hour, int minute, String ampm){
         hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
@@ -37,10 +26,6 @@ public class ClockDisplay12Hour
         setTime(hour, minute, ampm);
     }
     
-    //Implement a method timeTick that takes no parameters and returns nothing
-    //The method should increase the minute value by one each run
-    //The hours should increase when the minutes roll over
-    //The updateDisplay method should be called before finishing
     public void timeTick(){
         minutes.increment();
         if (minutes.getValue() == 0){
@@ -60,9 +45,6 @@ public class ClockDisplay12Hour
         updateDisplay();
     }
     
-    //Implement a method setTime that takes in 2 parameters, hour and minute
-    //The method should set the hours value and minutes value
-    //The updateDisplay method should be called before finishing
     public void setTime(int hour, int minute, String ampm){
         hours.setValue(hour);
         minutes.setValue(minute);
@@ -70,15 +52,10 @@ public class ClockDisplay12Hour
         updateDisplay();
     }
     
-    //Implement a method getTime that takes no parameter and returns a String
-    //The return String should be formatted as HH:MM and report out the current time
     public String getTime(){
         return display;
     }
 
-    //Implement a method updateDisplay that takes no parameters and returns nothing
-    //The method should update the displayString with the current time in a format
-    //  HH:MM
     public void updateDisplay(){
         display = hours.getDisplayValue() + ":" + minutes.getDisplayValue() + period;
     }
